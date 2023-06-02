4b17e3da48da6b7b0ec3ca2460665b86c2e30836
Sanjeeb Kumar Nayak <97420921+Sanjeeb-Kumar-Nayak@users.noreply.github.com>
2023-06-02 08:48:03 +0530

    change

--- express/user.js
+++ express/user.js
@@ -32,22 +32,31 @@ app.post("/createUser", (req, resp) => {
     "insert into users (email, mobile, name, password) values ($1, $2, $3, $4) returning *",
     [email, mobile, name, password],
     (err, result) => {
-      let data = { status: 1, message: "Success", data: result.rows };
+      let data = { status: 1, message: "Successfully User Created", data: result.rows };
       resp.send(data);
     }
   );
 });
 
-app.post("/updateUser", (req, resp) => {
+app.post("/updateUser/:id", (req, resp) => {
+  const id = parseInt(req.params.id);
   const { email, mobile, name, password } = req.body;
   connection.query(
-    "update users set email = $1, mobile = $2, name = $3, password = $4 where mobile = $5 returning *",
-    [email, mobile, name, password],
+    "update users set email = $1, mobile = $2, name = $3, password = $4 where id = $5",
+    [email, mobile, name, password, id],
     (err, result) => {
-      let data = { status: 1, message: "Success", data: result };
+      let data = { status: 1, message: "Successfully User Updated", data: result };
       resp.send(data);
     }
   );
 });
+
+app.post("/deleteUser/:id", (req,resp) => {
+  const id = parseInt(req.params.id);
+  connection.query("delete from users where id = $1", [id] ,(err, result) => {
+    let data = { status: 1, message: "Successfully User Deleted", data: result };
+      resp.send(data);
+  })
+})
 
 app.listen(8080);
