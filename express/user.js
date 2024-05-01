11fa26c30bf134dd8f43715d358088e49155f85c
Sanjeeb Kumar Nayak <97420921+Sanjeeb-Kumar-Nayak@users.noreply.github.com>
2024-05-01 13:24:47 +0530

    change

--- express/user.js
+++ express/user.js
@@ -22,7 +22,13 @@ let transporter = nodeMailer.createTransport({
 });
 
 const listingUser = (req, resp) => {
-  connection.query("select * from users", (err, result) => {
+  const { email, mobile, name } = req.body;
+  let query = "select * from users where 1=1";
+  if (email) query += ` and email = '${email}'`;
+  if (mobile) query += ` and mobile = '${mobile}'`;
+  if (name) query += ` and name = '${name}'`;
+
+  connection.query(query, (err, result) => {
     if (err) {
       let data = { status: 0, message: "Failed", data: result };
       resp.send(data);
