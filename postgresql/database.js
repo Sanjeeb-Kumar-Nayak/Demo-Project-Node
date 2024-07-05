07bbf790930f82a82ea6de52178960e42608edcf
Sanjeeb Kumar Nayak <97420921+Sanjeeb-Kumar-Nayak@users.noreply.github.com>
2024-07-05 08:50:35 +0530

    change

--- postgresql/database.js
+++ postgresql/database.js
@@ -1,12 +1,18 @@
-const {Client} = require('pg');
-const connection = new Client({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "tilu1994",
-    database: "system"
-})
+const { Client } = require("pg");
 
-connection.connect();
+const connection = new Client({
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "tilu1994",
+  database: "system",
+});
+
+connection
+  .connect()
+  .then(() => console.log("Connected to the postgresql database successfully"))
+  .catch((err) =>
+    console.error("Postgresql database connection error", err.stack)
+  );
 
 module.exports = connection;
